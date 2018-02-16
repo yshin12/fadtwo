02f94210c9e442e98eec9c94a655828b25ac528e
yshin12 <35882213+yshin12@users.noreply.github.com>
2018-02-15 22:33:28 -0500

    Add files via upload

--- lib_fadtwo.R
+++ lib_fadtwo.R
@@ -6,16 +6,17 @@
 # 
 
 
-fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.dt=1e06, L.gm=-1e06, U.gm=1e06, tau1=0.05, tau2=0.95, 
-                   eta=1e-6, params=list(OutputFlag=1, FeasibilityTol=1e-9),
-                   grid=NULL) {
+fadtwo <- function(y,x,f1, f2, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.dt=1e06, L.gm1=-1e06, U.gm1=1e06, L.gm2=-1e06, U.gm2=1e06, L.gm3=-1e06, U.gm3=1e06, 
+                   L.p, U.p, tau1=0.05, tau2=0.95, eta=1e-6, params=list(OutputFlag=1, FeasibilityTol=1e-9),
+                   grid=NULL, ld) {
 
   # Model: y = x'bt + x'dt * 1{f'gm > 0} + eps
   #
   # Input:
   #   y: outcome variable
   #   x: covariates
-  #   f: factors
+  #   f1: included factors
+  #   f2: factors to be selected
   #   L.bt / U.bt: Lower and upper bounds for bt. The dim should be equal to the dim of bt
   #   L.dt / U.dt: Lower and upper bounds for dt
   #   L.gm / U.gm: Lower and upper bounds for gm
@@ -25,15 +26,18 @@ fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.d
   #   params: parameters for gurobi engine
   #
   x = as.matrix(x)
-  f = as.matrix(f)
+  f1 = as.matrix(f1)
+  f2 = as.matrix(f2)
+  f = cbind(f1,f2, -1)
   n.obs = nrow(x)     # Number of observations
   d.x = ncol(x)       # Dimension of regressors
   d.f = ncol(f)       # Dimension of factors
-  
+  p = ncol(f2)
+
   if (method=="joint"){
     # Calculate Big-M  
     A.gm = c(1,-1) %x% diag(rep(1,d.f)) 
-    b.gm = c(U.gm, -L.gm)
+    b.gm = c(U.gm1, U.gm2, U.gm3, -L.gm1, -L.gm2, -L.gm3)
     M = rep(NA,n.obs)
     for (i in (1:n.obs)){
       M[i] = get_m(f=f[i,],A=A.gm,b=b.gm)$m
@@ -41,17 +45,18 @@ fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.d
     
     # Estimate parameters by solving the mixed integer quadratic programming
     # 1. Construct the objective function  
-    L.obj = get_L(x=x,y=y,L.dt=L.dt, d.f=d.f)
-    Q.obj = get_Q(x=x, L.dt=L.dt, d.f=d.f)
+    L.obj = get_L(x=x,y=y,L.dt=L.dt, d.f=d.f, ld=ld, p=p)
+    Q.obj = get_Q(x=x, L.dt=L.dt, d.f=d.f, p=p)
     objcon = mean(y^2)
     
     # 2. Build the constraints
-    const = build_constraint(L.bt, U.bt, L.dt, U.dt, L.gm, U.gm, M=M, eta=eta, d.x=d.x, d.f=d.f, n.obs=n.obs, f=f, tau1=tau1, tau2=tau2)
+    const = build_constraint(L.bt=L.bt, U.bt=U.bt, L.dt=L.dt, U.dt=U.dt, L.gm1=L.gm1, U.gm=U.gm1, L.gm2=L.gm2, U.gm2=U.gm2, L.gm3=L.gm3, U.gm3=U.gm3, L.p=L.p, U.p=U.p, M=M, eta=eta, 
+                             d.x=d.x, d.f=d.f, n.obs=n.obs, f=f, tau1=tau1, tau2=tau2, p=p)
     A.const = const$A.const
     b.const = const$b.const
     
     # 3. Estimate the model
-    result = estimate(y=y, x=x, f=f, Q.obj=Q.obj, L.obj=L.obj, objcon=objcon, A.const=A.const, b.const=b.const, L.bt=L.bt, L.gm=L.gm, params=params)
+    result = estimate(y=y, x=x, f=f, Q.obj=Q.obj, L.obj=L.obj, objcon=objcon, A.const=A.const, b.const=b.const, L.bt=L.bt, L.gm=L.gm, params=params, p=p)
     opt.par = result$x
     bt.hat = opt.par[1:d.x]
     l.hat = opt.par[(d.x+1):(d.x*(n.obs+1))]
@@ -59,8 +64,12 @@ fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.d
     dt.tilde = opt.par[((d.x*(n.obs+1))+ n.obs + 1):( (d.x*(n.obs+1))+ n.obs  + d.x )]
     # Note that dt.tilde = dt.hat - L.dt.  
     dt.hat = dt.tilde + L.dt  
-    gm.hat = opt.par[((d.x*(n.obs+1))+n.obs  + d.x + 1):((d.x*(n.obs+1))+n.obs  + d.x  + d.f)]
-    objval=get_objval(y,x,f,bt.hat,dt.hat,gm.hat, eta=1e-6)
+    gm1.hat = opt.par[((d.x*(n.obs+1))+n.obs  + d.x + 1):((d.x*(n.obs+1))+n.obs  + d.x  + d.f - p)]
+    gm2.hat = opt.par[((d.x*(n.obs+1))+n.obs  + d.x  + d.f - p + 1):((d.x*(n.obs+1))+n.obs  + d.x  + d.f)]
+    gm.hat = c(gm1.hat, gm2.hat)
+    e.hat = opt.par[((d.x*(n.obs+1))+n.obs  + d.x  + d.f + 1):((d.x*(n.obs+1))+n.obs  + d.x  + d.f + p)]
+    #objval=get_objval(y,x,f,bt.hat,dt.hat,gm.hat, eta=eta)
+    objval=result$objval
     cat('-----------------------------------------', '\n')
     cat('Results', '\n')
     cat('-----------------------------------------', '\n')
@@ -69,17 +78,16 @@ fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.d
     cat('Obj val     =', objval, '\n')
     cat('Beta.hat   =', bt.hat, '\n')
     cat('Delta.hat   =', dt.hat, '\n')
-    cat('Gamma.hat   =', gm.hat, '\n')
+    cat('Gamma1.hat   =', gm1.hat, '\n')
+    cat('Gammm2.hat   =', gm2.hat, '\n')
     cat('-----------------------------------------', '\n')
     # return(list(bt.hat=bt.hat, dt.hat=dt.hat, gm.hat=gm.hat, result.out=result))  # Output for detailed results
     return(list(bt.hat=bt.hat, dt.hat=dt.hat, gm.hat=gm.hat, objval=objval))
   }
   else if(method == "iter"){
-    cat('test: iterative','\n')
-    
     # Grid Search for gm
     n.grid=nrow(grid)
-    step1.out = step1_grid(y=y,x=x,f=f,grid=grid)
+    step1.out = step1_grid(y=y,x=x,f=f,grid=grid, eta=eta)
     ap.hat.step1 = step1.out$ap.hat
     gm.hat.step1 = step1.out$gm.hat
     cat('-----------------------------------------', '\n')
@@ -90,7 +98,7 @@ fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.d
 
     # Calculate Big-M  
     A.gm = c(1,-1) %x% diag(rep(1,d.f)) 
-    b.gm = c(U.gm, -L.gm)
+    b.gm = c(U.gm1, U.gm2, -L.gm1, -L.gm2)
     M = rep(NA,n.obs)
     for (i in (1:n.obs)){
       M[i] = get_m(f=f[i,],A=A.gm,b=b.gm)$m
@@ -103,10 +111,10 @@ fadtwo <- function(y,x,f, method="joint", L.bt=-1e06, U.bt=1e06, L.dt=-1e06, U.d
     d.hat = step2_1.out$d.t
     
     # Update ap.hat
-    step2_2.out = estimate_bt_dt(y=y, x=x, f=f, gm=gm.hat)
+    step2_2.out = estimate_bt_dt(y=y, x=x, f=f, gm=gm.hat, eta=eta)
     bt.hat = step2_2.out$bt.hat
     dt.hat = step2_2.out$dt.hat
-    objval=get_objval(y,x,f,bt.hat,dt.hat,gm.hat, eta=1e-6)
+    objval=get_objval(y,x,f,bt.hat,dt.hat,gm.hat, eta=eta)
     cat('-----------------------------------------', '\n')
     cat('Results', '\n')
     cat('-----------------------------------------', '\n')
@@ -208,7 +216,7 @@ get_m <-function(f,A,b,flag.print=FALSE){
 #
 # This function constructs a square matrix 'Q' for the quadratic term of the objective function
 #
-get_Q <-function(x, L.dt, d.f){
+get_Q <-function(x, L.dt, d.f, p){
   library('Matrix')
   
   T = nrow(as.matrix(x))
@@ -240,9 +248,9 @@ get_Q <-function(x, L.dt, d.f){
   Q = rbind(Q1,Q2, Q3) / T
   
   # Add additional 0 columns
-  Q = cbind(Q, matrix(0, nrow=nrow(Q), ncol=(d.x+d.f)))
+  Q = cbind(Q, matrix(0, nrow=nrow(Q), ncol=(d.x+d.f+p)))
   # Add additional 0 rows
-  Q = rbind(Q, matrix(0, nrow=(d.x+d.f), ncol=ncol(Q)))
+  Q = rbind(Q, matrix(0, nrow=(d.x+d.f+p), ncol=ncol(Q)))
   
   return(Q)
 }
@@ -251,7 +259,7 @@ get_Q <-function(x, L.dt, d.f){
 #
 # This function constructs a vector 'L' for the linear term of the objective function
 #
-get_L <-function(x, y, L.dt, d.f){
+get_L <-function(x, y, L.dt, d.f, ld, p){
   
   n.obs = length(y)
   y = as.matrix(y,n.obs,1)
@@ -267,7 +275,7 @@ get_L <-function(x, y, L.dt, d.f){
   
   A = cbind(A.1,A.2,A.3)
   A = (-2/n.obs) * A
-  L = cbind(A, matrix(0,1,(d.x+d.f)))
+  L = cbind(A, matrix(0,1,(d.x+d.f)), matrix(ld,1,p))
   
   return(L)
 }
@@ -277,14 +285,22 @@ get_L <-function(x, y, L.dt, d.f){
 #
 # This function constructs the whole linear constraints: Matrix of the LHS and a vector of RHS will be generated
 #
-build_constraint = function(L.bt, U.bt, L.dt, U.dt, L.gm, U.gm, M, eta, d.x, d.f, n.obs, f, tau1, tau2){
+# parameters = {bt,    l,   d,   dt,   gm1,   gm2,   e}
+#               d.x  d.x*T  T    d.x  d.f-p    p     p
+build_constraint = function(L.bt, U.bt, L.dt, U.dt, L.gm1, U.gm1, L.gm2, U.gm2, L.gm3, U.gm3, L.p, U.p, M, eta, d.x, d.f, n.obs, f, tau1, tau2, p){
   
   # Build the RHS vector of constatins
   
   # constraints #1
   b.const = c(U.bt, -L.bt)
   # constraints #2
-  b.const = c(b.const, U.gm, -L.gm)
+  b.const = c(b.const, U.gm1, -L.gm1)
+  # constraints #2.1
+  b.const = c(b.const, rep(0, p))
+  # constraints #2.2
+  b.const = c(b.const, rep(0, p))
+  # constraints #2.3
+  b.const = c(b.const, U.gm3, -L.gm3)
   # constraints #3
   b.const = c(b.const, (U.dt - L.dt), rep(0,d.x))
   # constraints #4
@@ -307,20 +323,50 @@ build_constraint = function(L.bt, U.bt, L.dt, U.dt, L.gm, U.gm, M, eta, d.x, d.f
   b.const = c(b.const, -tau1)
   # constraints #10
   b.const = c(b.const, tau2)
+  # constraints #11
+  b.const = c(b.const, -L.p)
+  # constraints #12
+  b.const = c(b.const, U.p)
   
   
   # Build the LHS matrix
-  ncol.A = d.x + d.x*n.obs + n.obs + d.x + d.f 
+  ncol.A = d.x + d.x*n.obs + n.obs + d.x + d.f + p
   I.dx = diag(d.x)
   I.df = diag(d.f)
+  d.f.gm1 = d.f -  p - 1
+  I.df.gm1 = diag(d.f.gm1)
+  I.df.gm2 = I.p = diag(p)
   
   # const #1: beta upper bounds
   A1 = rbind(I.dx, -I.dx)
   A1 = cbind(A1, matrix(0, nrow=nrow(A1), ncol=ncol.A-d.x))
   
-  # const #2: gamma upper bounds
-  A2 = rbind(I.df, -I.df)
-  A2 = cbind(matrix(0, nrow=nrow(A2), ncol=(ncol.A - d.f)), A2 )
+  # const #2: gamma1 bounds
+  A2 = rbind(I.df.gm1, -I.df.gm1)
+  A2 = cbind(matrix(0, nrow=nrow(A2), ncol=(d.x+d.x*n.obs+n.obs+d.x)), A2, matrix(0, nrow=nrow(A2), ncol=2*p+1) )
+
+  #------------------------------------------------------------------------------------------------
+  #
+  # constraints 2.1 -- 2.3 are added for model selection
+  #
+  #------------------------------------------------------------------------------------------------
+  # const #2.1: gm2 - U.gm2*e <= 0
+  I.U.gm2 = I.df.gm2
+  diag(I.U.gm2) = U.gm2
+  A2.1 = cbind(I.df.gm2, 0, -I.U.gm2)  # Order: gm2, gm3 (for constant term -1), e 
+  A2.1 = cbind(matrix(0, nrow=nrow(A2.1), ncol=(d.x+d.x*n.obs+n.obs+d.x+d.f.gm1)), A2.1)
+
+  # const #2.2: - gm2 + L.gm2*e <= 0
+  I.L.gm2 = I.df.gm2
+  diag(I.L.gm2) = L.gm2
+  A2.2 = cbind(-I.df.gm2, 0, I.L.gm2)
+  A2.2 = cbind(matrix(0, nrow=nrow(A2.2), ncol=(d.x+d.x*n.obs+n.obs+d.x+d.f.gm1)), A2.2)
+
+  # const #2.3: gamma3 bounds
+  A2.3 = rbind(1, -1)
+  A2.3 = cbind(matrix(0, nrow=nrow(A2.3), ncol=(d.x+d.x*n.obs+n.obs+d.x+d.f.gm1+p)), A2.3, matrix(0, nrow=nrow(A2.3), ncol=p) )
+
+  #------------------------------------------------------------------------------------------------
   
   # const #3: delta.tilde upper bounds
   A3 = rbind(I.dx, -I.dx)
@@ -331,55 +377,70 @@ build_constraint = function(L.bt, U.bt, L.dt, U.dt, L.gm, U.gm, M, eta, d.x, d.f
   # RHS is l.tilde - dt.tilde <= 0 
   A4.1 = I.dx %x% diag(n.obs) 
   A4.2 = -rep(1,n.obs) %x% I.dx 
-  A4 = cbind(matrix(0, nrow=nrow(A4.1), ncol=d.x), A4.1, matrix(0, nrow=nrow(A4.1), ncol=n.obs), A4.2, matrix(0, nrow=nrow(A4.1), ncol=d.f) )
+  A4 = cbind(matrix(0, nrow=nrow(A4.1), ncol=d.x), A4.1, matrix(0, nrow=nrow(A4.1), ncol=n.obs), A4.2, matrix(0, nrow=nrow(A4.1), ncol=d.f+p) )
   
   # const #4.5: 0 <= l.tilde <= dt.tilde  
   # LHS inequality: -l.tilde <= 0
   A4.11 = -I.dx %x% diag(n.obs) 
-  A4.5 = cbind(matrix(0, nrow=nrow(A4.11), ncol=d.x), A4.11, matrix(0, nrow=nrow(A4.11), ncol=n.obs), matrix(0, nrow=nrow(A4.11), ncol=d.x) , matrix(0, nrow=nrow(A4.11), ncol=d.f) )
+  A4.5 = cbind(matrix(0, nrow=nrow(A4.11), ncol=d.x), A4.11, matrix(0, nrow=nrow(A4.11), ncol=n.obs), matrix(0, nrow=nrow(A4.11), ncol=d.x) , 
+               matrix(0, nrow=nrow(A4.11), ncol=(d.f+p) ) )
   
   
   # const #5: Left inequality of f_t'gm
   A5.1 = diag(M+2*eta)
   A5.2 = -f
-  A5 = cbind(matrix(0, nrow=nrow(A5.1), ncol=d.x), matrix(0, nrow=nrow(A5.1), ncol=n.obs*d.x), A5.1, matrix(0, nrow=nrow(A5.1), ncol=d.x), A5.2 )
+  A5 = cbind(matrix(0, nrow=nrow(A5.1), ncol=d.x), matrix(0, nrow=nrow(A5.1), ncol=n.obs*d.x), A5.1, matrix(0, nrow=nrow(A5.1), ncol=d.x), A5.2, matrix(0, nrow=nrow(A5.1), ncol=p))
   
   # const #6: Right inequality of f_t'gm
   A6.1 = diag(-M) 
   A6.2 = f
-  A6 = cbind(matrix(0, nrow=nrow(A6.1), ncol=d.x), matrix(0, nrow=nrow(A6.1), ncol=n.obs*d.x), A6.1, matrix(0, nrow=nrow(A6.1), ncol=d.x), A6.2 )
+  A6 = cbind(matrix(0, nrow=nrow(A6.1), ncol=d.x), matrix(0, nrow=nrow(A6.1), ncol=n.obs*d.x), A6.1, matrix(0, nrow=nrow(A6.1), ncol=d.x), A6.2, matrix(0, nrow=nrow(A5.1), ncol=p) )
   
   # const #7: Right inequality sum l_t <= d_t * sum (U.dt - L.dt)
   sum.bnd = sum(U.dt - L.dt)
   A7.1 = diag(n.obs) %x% t(rep(1,d.x))
   A7.2 = (-sum.bnd)*diag(n.obs) 
-  A7 = cbind(matrix(0, nrow=nrow(A7.1), ncol=d.x), A7.1, A7.2, matrix(0, nrow=nrow(A7.1), ncol=d.x), matrix(0, nrow=nrow(A7.1), ncol=d.f) )
+  A7 = cbind(matrix(0, nrow=nrow(A7.1), ncol=d.x), A7.1, A7.2, matrix(0, nrow=nrow(A7.1), ncol=d.x), matrix(0, nrow=nrow(A7.1), ncol=d.f+p) )
   
   # const #7.5: Left inequality [- sum l_t] <= 0
   A7.3 = diag(n.obs) %x% t(rep(-1,d.x))
-  A7.5 = cbind(matrix(0, nrow=nrow(A7.3), ncol=d.x), A7.3, matrix(0, nrow=nrow(A7.3), ncol=n.obs), matrix(0, nrow=nrow(A7.3), ncol=d.x), matrix(0, nrow=nrow(A7.3), ncol=d.f) )
+  A7.5 = cbind(matrix(0, nrow=nrow(A7.3), ncol=d.x), A7.3, matrix(0, nrow=nrow(A7.3), ncol=n.obs), matrix(0, nrow=nrow(A7.3), ncol=d.x), matrix(0, nrow=nrow(A7.3), ncol=d.f+p) )
   
   
   # const #8: Right inequality of sum (l - delta)
   A8.1 = - diag(n.obs) %x% t(rep(1,d.x))
   A8.2 = sum.bnd*diag(n.obs)
   A8.3 = rep(1,n.obs)%x% t(rep(1,d.x))
-  A8 = cbind(matrix(0, nrow=nrow(A8.1), ncol=d.x), A8.1, A8.2, A8.3, matrix(0, nrow=nrow(A8.1), ncol=d.f) )
+  A8 = cbind(matrix(0, nrow=nrow(A8.1), ncol=d.x), A8.1, A8.2, A8.3, matrix(0, nrow=nrow(A8.1), ncol=d.f+p) )
   
   # const #8.5: Left inequality of sum (l - delta)
   A8.11 = diag(n.obs) %x% t(rep(1,d.x))
   A8.33 = - rep(1,n.obs)%x% t(rep(1,d.x))
-  A8.5 = cbind(matrix(0, nrow=nrow(A8.11), ncol=d.x), A8.11, matrix(0, nrow=nrow(A8.11), ncol=n.obs), A8.33, matrix(0, nrow=nrow(A8.11), ncol=d.f) )
+  A8.5 = cbind(matrix(0, nrow=nrow(A8.11), ncol=d.x), A8.11, matrix(0, nrow=nrow(A8.11), ncol=n.obs), A8.33, matrix(0, nrow=nrow(A8.11), ncol=d.f+p) )
   
   # const #9: Left inequality of sum d_t
   A9 = matrix(-rep(1/n.obs,n.obs), 1, n.obs)
-  A9 = cbind(matrix(0, nrow=1, ncol=d.x), matrix(0, nrow=1, ncol=d.x*n.obs), A9, matrix(0, nrow=1, ncol=d.x),  matrix(0, nrow=1, ncol=d.f) )
+  A9 = cbind(matrix(0, nrow=1, ncol=d.x), matrix(0, nrow=1, ncol=d.x*n.obs), A9, matrix(0, nrow=1, ncol=d.x),  matrix(0, nrow=1, ncol=d.f+p) )
   
   # const #10: Right inequality of sum d_t
   A10 = matrix(rep(1/n.obs,n.obs), 1, n.obs)
-  A10 = cbind(matrix(0, nrow=1, ncol=d.x), matrix(0, nrow=1, ncol=d.x*n.obs), A10, matrix(0, nrow=1, ncol=d.x),  matrix(0, nrow=1, ncol=d.f) )
+  A10 = cbind(matrix(0, nrow=1, ncol=d.x), matrix(0, nrow=1, ncol=d.x*n.obs), A10, matrix(0, nrow=1, ncol=d.x),  matrix(0, nrow=1, ncol=d.f+p) )
   
-  A.const=rbind(A1,A2,A3,A4,A4.5, A5,A6,A7,A7.5, A8, A8.5, A9, A10)
+  #------------------------------------------------------------------------------------------------
+  #
+  # constraints 11 & 12 are added for model selection
+  #
+  #------------------------------------------------------------------------------------------------
+  # const #11: Left inequality of sum e_t
+  A11 = matrix(1, 1, p)
+  A11 = cbind(matrix(0, nrow=nrow(A11), ncol=(ncol.A-p)), -A11)
+
+  # const #12: Right inequality of sum e_t
+  A12 = matrix(1, 1, p)
+  A12 = cbind(matrix(0, nrow=nrow(A11), ncol=(ncol.A-p)), A12)
+  #------------------------------------------------------------------------------------------------
+  
+  A.const=rbind(A1,A2,A2.1,A2.2,A2.3,A3,A4,A4.5, A5,A6,A7,A7.5, A8, A8.5, A9, A10, A11, A12)
   
   return(list(A.const=A.const,b.const=b.const))
 }
@@ -390,7 +451,7 @@ build_constraint = function(L.bt, U.bt, L.dt, U.dt, L.gm, U.gm, M, eta, d.x, d.f
 #
 # This function gives the estimation results
 #
-estimate <- function (y, x, f, Q.obj, L.obj, objcon, A.const, b.const, L.bt, L.gm, params) {
+estimate <- function (y, x, f, Q.obj, L.obj, objcon, A.const, b.const, L.bt, L.gm, params, p) {
   
   # Call Library
   library("gurobi")
@@ -409,10 +470,9 @@ estimate <- function (y, x, f, Q.obj, L.obj, objcon, A.const, b.const, L.bt, L.g
   model$A       = A.const
   model$rhs     = b.const
   model$sense   = rep('<=', length(model$rhs))
-  model$lb      = c(L.bt, rep(0,d.x*n.obs), rep(0,n.obs),  rep(0,d.x), L.gm)         #Done
-  #model$lb      = c(rep(-10^10,d.x), rep(-10^10,d.x*n.obs), rep(-10^10,n.obs),  rep(-10^10,d.x), rep(-10^10,d.f))         #Done
+  model$lb      = c(L.bt, rep(0,d.x*n.obs), rep(0,n.obs),  rep(0,d.x), L.gm1, L.gm2, L.gm3, rep(0,p))
   
-  model$vtype   = c(rep('C', d.x), rep('C', d.x*n.obs), rep('B',n.obs), rep('C', d.x), rep('C', d.f))   
+  model$vtype   = c(rep('C', d.x), rep('C', d.x*n.obs), rep('B',n.obs), rep('C', d.x), rep('C', d.f-p), rep('C', p), rep('B', p))   
   model$modelsense = 'min'
   result <- gurobi(model, params=params)
 
@@ -424,13 +484,13 @@ estimate <- function (y, x, f, Q.obj, L.obj, objcon, A.const, b.const, L.bt, L.g
 #
 # This function estimates gm by the grid search method
 #
-step1_grid <- function(y, x, f, grid){
+step1_grid <- function(y, x, f, grid, eta=1e-6){
   n.grid = nrow(grid)
   dim.x = ncol(x)
   result = matrix(NA, nrow=n.grid, ncol=(1+2*dim.x))   # Collect ap.hat and the objective function value
   for (i in (1:n.grid)){
     f.index = as.numeric(f %*% t(grid[i,]))
-    x.reg = cbind(x, x*(f.index>0))
+    x.reg = cbind(x, x*(f.index>eta*0.99))
     m = lm(y~x.reg-1)
     result[i,] = c(sum(m$resid^2),coef(m))
     if (is.na(sum(coef(m)))) {
@@ -449,11 +509,11 @@ step1_grid <- function(y, x, f, grid){
 #
 # This function estimate bt and dt by OLS given an estimate for gm
 #
-estimate_bt_dt <- function(y, x, f, gm){
+estimate_bt_dt <- function(y, x, f, gm, eta=1e-6){
   T = length(y)                             # Sample size
   dim.bt = dim.dt = ncol(x)                 # The number of columns in x, size of Beta and Delta
   
-  indicator = (f %*% gm > 0)                # Construct the indicator function with gm given
+  indicator = (f %*% gm >= eta*0.99)                # Construct the indicator function with gm given
   x.ind = x * matrix(indicator, T, dim.bt)  # Construct regressors for Delta
   reg = cbind(x, x.ind)                     # Construct the whole regressors for Beta and Delta
   m1 = lm(y~-1+reg)                         # OLS and save the model as m1
@@ -534,10 +594,11 @@ estimate_gm <- function(y, x, f, bt, dt, A, b, M,  tau1, tau2, params, eta, ...)
 #
 get_objval = function(y,x,f,bt,dt,gm, eta=1e-6) {
   n.obs=length(y)
-  index = f%*%gm >= eta
+  index = f%*%gm >= eta*0.99
   y.hat = x%*%bt + (x%*%dt) * index
   u.hat = y - y.hat
   objval = (t(u.hat) %*% u.hat)/n.obs
   
   return(objval)
 }
+
